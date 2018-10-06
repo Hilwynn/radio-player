0a3e2c0ee0dc1e0641f1a4d49eea93fc32d622dd
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-06 17:39:59 +0200

    Removed TransitionGroups + got the skeleton loaders to work

--- code/src/components/Station/index.js
+++ code/src/components/Station/index.js
@@ -7,8 +7,10 @@ class Station extends React.Component {
       color,
       image,
       name,
+      stations,
       url
     } = this.props
+    const availableStations = stations.length
     const channelColor = {
       backgroundColor: {
         backgroundColor: `#${color}`
@@ -16,18 +18,24 @@ class Station extends React.Component {
     }
 
     return (
-      <div className="station-container" style={channelColor.backgroundColor}>
-        <div className="station-image">
-          <img src={image} alt={name} />
-        </div>
-        <div className="station-body">
-          <div>
-            <audio controls>
-              <source src={url} type="audio/mpeg" />
-              Your browser does not support the audio tag.
-            </audio>
+      <div>
+        {availableStations > 0 ? (
+          <div className="station-container" style={channelColor.backgroundColor}>
+            <div className="station-image">
+              <img src={image} alt={name} />
+            </div>
+            <div className="station-body">
+              <div>
+                <audio controls>
+                  <source src={url} type="audio/mpeg" />
+                  Your browser does not support the audio tag.
+                </audio>
+              </div>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="empty-station" />
+        )}
       </div>
     )
   }
--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,8 +1,5 @@
 import React from "react"
-import TransitionGroup from "react-transition-group/TransitionGroup"
-import Transition from "react-transition-group/Transition"
 import Station from "./Station"
-import StationSkeleton from "./StationSkeleton"
 
 class App extends React.Component {
   constructor(props) {
@@ -24,27 +21,16 @@ class App extends React.Component {
 
   render() {
     return (
-      <TransitionGroup>
-        {!this.state.radioStations.length > 0 ? (
-          <Transition key="loading" timeout={250}>
-            {state => <StationSkeleton data-state={state} />}
-          </Transition>
-        ) : (
-          <Transition key="data" timeout={250}>
-            {state => (
-              <div>
-                {this.state.radioStations.map(channel =>
-                  <Station
-                    key={channel.id}
-                    color={channel.color}
-                    image={channel.image}
-                    name={channel.name}
-                    url={channel.liveaudio.url} />)}
-              </div>
-            )}
-          </Transition>
-        )}
-      </TransitionGroup>
+      <div>
+        {this.state.radioStations.map(channel =>
+          <Station
+            key={channel.id}
+            color={channel.color}
+            image={channel.image}
+            name={channel.name}
+            stations={this.state.radioStations}
+            url={channel.liveaudio.url} />)}
+      </div>
     )
   }
 }
