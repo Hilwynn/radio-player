2252d28d175d5da38a27d8ad39ebe3fbe7e42d8b
Anna Erkers <31205429+Hilwynn@users.noreply.github.com>
2018-10-16 14:06:49 +0200

    Updates

---
+++ precache-manifest.3de51ce316fe0e4707a9a59e0d8bf734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a6e191e94737544383",
+    "url": "/radio-player/static/js/runtime~main.12a6e191.js"
+  },
+  {
+    "revision": "96fe50f0180a22c4f2ba",
+    "url": "/radio-player/static/js/main.96fe50f0.chunk.js"
+  },
+  {
+    "revision": "8df7078f39324016d415",
+    "url": "/radio-player/static/js/1.8df7078f.chunk.js"
+  },
+  {
+    "revision": "96fe50f0180a22c4f2ba",
+    "url": "/radio-player/static/css/main.78863ab8.chunk.css"
+  },
+  {
+    "revision": "1470f76da692f6d9f455984ece582569",
+    "url": "/radio-player/index.html"
+  }
+];
